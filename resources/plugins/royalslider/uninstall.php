c600e6be55d2b8cf9a594c4f01370f11ca5f340d
Gundsambuu Natsagdorj <ssxenon01@gmail.com>
2014-04-15 16:38:47 +0800

    Slider added.

--- resources/plugins/royalslider/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php 
-if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
-	exit();
-
-global $wpdb;
-$sliders_table = $wpdb->prefix . 'royalsliders';
-$wpdb->query( "DROP TABLE $sliders_table" );
-
-?>
