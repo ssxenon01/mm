c9084efdba9c2b9aaf835d772189010d220e62c2
Gundsambuu Natsagdorj <ssxenon01@gmail.com>
2014-04-23 14:04:02 +0800

    орчуулга зассан

--- resources/plugins/sabai-directory/assets/templates/directory_custom_fields.html.php
+++ resources/plugins/sabai-directory/assets/templates/directory_custom_fields.html.php
@@ -1,6 +1,11 @@
 <?php foreach ($this->Entity_CustomFields($entity) as $field):?>
+    <?php $fieldName =  str_replace('_', '-', $field['name']);
+    if($fieldName == 'field-with-whom' || $fieldName == 'field-what'){
+        return '';
+    }
+    ?>
 <?php   if ($field_output = $this->Entity_RenderField($entity, $field['type'], $field['settings'], $field['values'])):?>
-    <div class="sabai-directory-field sabai-field-type-<?php echo str_replace('_', '-', $field['type']);?> sabai-field-name-<?php echo str_replace('_', '-', $field['name']);?> sabai-clearfix">
+    <div class="sabai-directory-field sabai-field-type-<?php echo str_replace('_', '-', $field['type']);?> sabai-field-name-<?php echo $fieldName;?> sabai-clearfix">
         <div class="sabai-field-label"><?php Sabai::_h($field['title']);?></div>
         <div class="sabai-field-value"><?php echo $field_output;?></div>
     </div>
