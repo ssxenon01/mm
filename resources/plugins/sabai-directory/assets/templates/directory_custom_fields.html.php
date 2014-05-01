b22acb4890a7d7a0171666a774bb87d956b69a80
Gundsambuu Natsagdorj <ssxenon01@gmail.com>
2014-05-01 21:23:57 +0800

    Updated

--- resources/plugins/sabai-directory/assets/templates/directory_custom_fields.html.php
+++ resources/plugins/sabai-directory/assets/templates/directory_custom_fields.html.php
@@ -1,10 +1,6 @@
 <?php foreach ($this->Entity_CustomFields($entity) as $field):?>
-    <?php $fieldName =  str_replace('_', '-', $field['name']);
-    if($fieldName == 'field-with-whom' || $fieldName == 'field-what' || $fieldName == 'field-location'){
-        return '';
-    }
-    ?>
-<?php   if ($field_output = $this->Entity_RenderField($entity, $field['type'], $field['settings'], $field['values'])):?>
+    <?php $fieldName =  str_replace('_', '-', $field['name']);?>
+<?php   if ( ($fieldName != 'field-with-whom' || $fieldName != 'field-what' || $fieldName != 'field-location') && $field_output = $this->Entity_RenderField($entity, $field['type'], $field['settings'], $field['values'])):?>
     <div class="sabai-directory-field sabai-field-type-<?php echo str_replace('_', '-', $field['type']);?> sabai-field-name-<?php echo $fieldName;?> sabai-clearfix">
         <div class="sabai-field-label"><?php Sabai::_h($field['title']);?></div>
         <div class="sabai-field-value"><?php echo $field_output;?></div>
