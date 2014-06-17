49365ed2ca08a15dc89e97a19d8a52ac3074e15e
Gundsambuu Natsagdorj <ssxenon01@gmail.com>
2014-06-17 12:07:16 +0800

    New design implementation

--- wp-admin/index.php
+++ wp-admin/index.php
@@ -34,33 +34,33 @@ $help = '<p>' . __( 'Welcome to your WordPress Dashboard! This is the screen you
 // Not using chaining here, so as to be parseable by PHP4.
 $screen = get_current_screen();
 
-$screen->add_help_tab( array(
+/*$screen->add_help_tab( array(
 	'id'      => 'overview',
 	'title'   => __( 'Overview' ),
 	'content' => $help,
-) );
+) );*/
 
 // Help tabs
 
 $help  = '<p>' . __( 'The left-hand navigation menu provides links to all of the WordPress administration screens, with submenu items displayed on hover. You can minimize this menu to a narrow icon strip by clicking on the Collapse Menu arrow at the bottom.' ) . '</p>';
 $help .= '<p>' . __( 'Links in the Toolbar at the top of the screen connect your dashboard and the front end of your site, and provide access to your profile and helpful WordPress information.' ) . '</p>';
 
-$screen->add_help_tab( array(
+/*$screen->add_help_tab( array(
 	'id'      => 'help-navigation',
 	'title'   => __( 'Navigation' ),
 	'content' => $help,
-) );
+) );*/
 
 $help  = '<p>' . __( 'You can use the following controls to arrange your Dashboard screen to suit your workflow. This is true on most other administration screens as well.' ) . '</p>';
 $help .= '<p>' . __( '<strong>Screen Options</strong> - Use the Screen Options tab to choose which Dashboard boxes to show.' ) . '</p>';
 $help .= '<p>' . __( '<strong>Drag and Drop</strong> - To rearrange the boxes, drag and drop by clicking on the title bar of the selected box and releasing when you see a gray dotted-line rectangle appear in the location you want to place the box.' ) . '</p>';
 $help .= '<p>' . __( '<strong>Box Controls</strong> - Click the title bar of the box to expand or collapse it. Some boxes added by plugins may have configurable content, and will show a &#8220;Configure&#8221; link in the title bar if you hover over it.' ) . '</p>';
 
-$screen->add_help_tab( array(
+/*$screen->add_help_tab( array(
 	'id'      => 'help-layout',
 	'title'   => __( 'Layout' ),
 	'content' => $help,
-) );
+) );*/
 
 $help  = '<p>' . __( 'The boxes on your Dashboard screen are:' ) . '</p>';
 if ( current_user_can( 'edit_posts' ) )
@@ -75,7 +75,7 @@ else
 if ( current_user_can( 'edit_theme_options' ) )
 	$help .= '<p>' . __( '<strong>Welcome</strong> - Shows links for some of the most common tasks when setting up a new site.' ) . '</p>';
 
-$screen->add_help_tab( array(
+/*$screen->add_help_tab( array(
 	'id'      => 'help-content',
 	'title'   => __( 'Content' ),
 	'content' => $help,
@@ -87,7 +87,7 @@ $screen->set_help_sidebar(
 	'<p><strong>' . __( 'For more information:' ) . '</strong></p>' .
 	'<p>' . __( '<a href="http://codex.wordpress.org/Dashboard_Screen" target="_blank">Documentation on Dashboard</a>' ) . '</p>' .
 	'<p>' . __( '<a href="https://wordpress.org/support/" target="_blank">Support Forums</a>' ) . '</p>'
-);
+);*/
 
 include( ABSPATH . 'wp-admin/admin-header.php' );
 ?>
