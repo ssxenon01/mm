5511564c3d5f21fa957e7cb72cf8cdf868453868
Gundsambuu Natsagdorj <ssxenon01@gmail.com>
2014-08-14 15:14:52 +0800

    mag implemented.

--- resources/themes/menu/sabai.php
+++ resources/themes/menu/sabai.php
@@ -5,7 +5,7 @@
 $img = $_GET['img_id'];
 if(!$img)
 
-    header("Location: http://localhost:8888/mymenu/resources/plugins/sabai/assets/images/no_image_small.png");
+    header("Location: http://mymenu.mn/resources/plugins/sabai/assets/images/no_image_small.png");
 
 else{
 
@@ -15,10 +15,10 @@ else{
     $id = $wpdb->get_row("SELECT f.file_name as id FROM menu_sabai_entity_field_file_image a , menu_sabai_file_file f WHERE f.file_id = a.file_id AND a.entity_id = $img " , OBJECT)->id;
 
     if($id){
-        header("Location: http://localhost:8888/mymenu/resources/sabai/File/thumbnails/$id");
+        header("Location: http://mymenu.mn/resources/sabai/File/thumbnails/$id");
         die();
     }else{
-        header("Location: http://localhost:8888/mymenu/resources/plugins/sabai/assets/images/no_image_small.png");
+        header("Location: http://mymenu.mn/resources/plugins/sabai/assets/images/no_image_small.png");
         die();
     }
 
